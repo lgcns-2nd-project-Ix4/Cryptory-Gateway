f9289d999cd9dd1ac35c269f62b30d7a6aa7536a
gwangbu-desu <kwy1379@ajou.ac.kr>
2025-03-30 02:21:28 +0900

    Feat: Config Bus 적용

---
+++ src/main/java/com/cryptory/cryptorygateway/CryptoryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cryptory.cryptorygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptory/cryptorygateway/CryptoryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptory.cryptorygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
